89fa5df60f0b22039129168032eddaa4ffe5af94
MarcGuiot <marc.guiot@free.fr>
2020-05-05 18:42:29 +0200

    split importFile to retrieve the current GlobType before read it Add manage of isSet in accept => only set field are accepted => Huge impact 15% on perf of json serialisation.

--- src/test/java/org/globsframework/commandline/ParseCommandLineTest.java
+++ src/test/java/org/globsframework/commandline/ParseCommandLineTest.java
@@ -3,11 +3,16 @@ package org.globsframework.commandline;
 import org.globsframework.metamodel.GlobType;
 import org.globsframework.metamodel.GlobTypeLoaderFactory;
 import org.globsframework.metamodel.annotations.DefaultInteger;
+import org.globsframework.metamodel.annotations.FieldNameAnnotation;
 import org.globsframework.metamodel.fields.IntegerField;
+import org.globsframework.metamodel.fields.StringArrayField;
 import org.globsframework.metamodel.fields.StringField;
 import org.globsframework.model.Glob;
+import org.globsframework.utils.Strings;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Arrays;
 
 public class ParseCommandLineTest {
 
@@ -22,14 +27,20 @@ public class ParseCommandLineTest {
         Assert.assertEquals("a name", opt.get(Opt1.NAME));
         Assert.assertEquals(321, opt.get(Opt1.VAL).intValue());
         Assert.assertArrayEquals(args, ParseCommandLine.toArgs(opt));
+
+        opt = ParseCommandLine.parse(Opt1.TYPE, new String[]{"--value", "1", "--value", "2"});
+        String[] strings = opt.getOrEmpty(Opt1.MULTIVALUES);
+        Assert.assertEquals(2, strings.length);
+        Assert.assertEquals("1,2", String.join(",", strings));
     }
-
-
 
     public static class Opt1 {
         public static GlobType TYPE;
 
         public static StringField NAME;
+
+        @FieldNameAnnotation("value")
+        public static StringArrayField MULTIVALUES;
 
         @DefaultInteger(123)
         public static IntegerField VAL;
